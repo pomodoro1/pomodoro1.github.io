09abb2b7d2e6ae969a927045468433325b4a4d62
pomodoro1 <joshziman@gmail.com>
2017-07-13 15:18:08 -0500

    import from bb dir

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+//gulpfile.js
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+//const bourbon = require('bourbon');
+
+//style paths
+var sassFiles = 'assets/styles/sass/**/*.scss',
+    cssDest = 'assets/styles/css/';
+
+gulp.task('styles', function(){
+    gulp.src(sassFiles)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(cssDest));
+});
+
+gulp.task('watch',function() {
+    gulp.watch(sassFiles,['styles']);
+});
